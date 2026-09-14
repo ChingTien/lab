f211b259e6ad65c560ad4fe6dae9174b5cc2148e
C <cognitiveching@gmail.com>
2026-09-14 17:27:50 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs.plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/',
+  base: '/lab/',
   server: { proxy: { '/api': 'http://127.0.0.1:8787' } },
 });
